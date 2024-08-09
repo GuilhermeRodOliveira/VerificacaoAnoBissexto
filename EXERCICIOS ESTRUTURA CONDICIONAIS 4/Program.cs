ee572abb0fce169ae75527d7ba07d580ad7693bf
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-09 20:57:07 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIOS ESTRUTURA CONDICIONAIS 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Informa se o ano é bissexto ou não.
+        Console.WriteLine("Digite o ano que você deseja verificar:");
+        int ano = int.Parse(Console.ReadLine());
+
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+        {
+            Console.WriteLine($"{ano} é um ano bissexto.");
+        }
+        else
+        {
+            Console.WriteLine($"{ano} não é um ano bissexto.");
+        }
+    }
+}
